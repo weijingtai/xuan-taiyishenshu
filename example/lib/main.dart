a92042b0fadfd4e4c5142fb2d0ce542aa2ddb4d4
wjt <wjt19951215@gmail.com>
2026-06-06 19:04:16 -0700

    refactor(taiyishenshu-storage): TYSS-22e wire example main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:taiyishenshu/theme/taiyi_classic_theme.dart';
 import 'deity_dialog_demo.dart';
+import 'taiyi_host.dart';
 
 void main() => runApp(const ExampleApp());
 
